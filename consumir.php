9d9ce873724460d604003cc2fa3e5edf20601260
unknown <coderschars289@gmail.com>
2023-06-02 04:33:59 -0600

    02062022

---
+++ consumir.php
@@ -0,0 +1,43 @@
+<?php            
+
+//     $ch= curl_init();
+
+// curl_setopt($ch, CURLOPT_URL,'https://reqres.in/api/users/');
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER,TRUE);
+
+// $response=curl_exec($ch);
+
+// if(curl_errno($ch)) echo curl_errno($ch);
+
+//     else
+//         $decoded = json_decode($response,true); 
+
+//         var_dump($decoded);
+       
+// curl_close($ch);     
+          
+    // echo"<table border=1>";
+    // echo"<tr>";
+    // echo"<td>id</td>";    
+    // echo"<td>email</td>";
+    // echo"<td>first_name</td>";
+    // echo"<td>last_name</td>";
+    // echo"<td>avatar</td>";
+    // echo"</tr>";
+
+    // foreach($decoded as $deco =>$value){           
+    // var_dump($deco);
+    // echo"<tr>";
+    // echo"<td>$deco</td>";    
+    // echo"<td>$deco</td>";
+    // echo"<td>$deco</td>";
+    // echo"<td>$deco</td>";
+    // echo"<td>$deco</td>";
+    // }
+    // echo"</tr>";
+
+
+    
+    ?>                     
+
+    
---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+
+$dbhost = "localhost" ;
+$dbuser = "root";
+$dbpass = "";
+$dbname ="consumirapi";
+
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+if(!$conn){
+    die("No hay conexión: ".mysqli_connect_error());
+
+}
+
+$nombre=$_POST["usuario"];
+$pass=$_POST["password"];
+
+$query = mysqli_query($conn, "SELECT * FROM login WHERE usuario='".$nombre."' and password = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if($nr==1){
+echo "Bienvenido:".$nombre;
+}
+else if($nr==0){
+    echo "NO SE PUDO INGRESAR";
+}
+
+
+
+
+?>
