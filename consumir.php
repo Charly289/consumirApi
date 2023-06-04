1dbd2b153dbfcc5dfcab33d9e0511724b71644b6
unknown <coderschars289@gmail.com>
2023-06-04 01:24:39 -0600

    04062023

--- consumir.php
+++ consumir.php
@@ -1,43 +1,50 @@
-<?php            
-
-//     $ch= curl_init();
-
-// curl_setopt($ch, CURLOPT_URL,'https://reqres.in/api/users/');
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER,TRUE);
-
-// $response=curl_exec($ch);
-
-// if(curl_errno($ch)) echo curl_errno($ch);
-
-//     else
-//         $decoded = json_decode($response,true); 
-
-//         var_dump($decoded);
-       
-// curl_close($ch);     
-          
-    // echo"<table border=1>";
-    // echo"<tr>";
-    // echo"<td>id</td>";    
-    // echo"<td>email</td>";
-    // echo"<td>first_name</td>";
-    // echo"<td>last_name</td>";
-    // echo"<td>avatar</td>";
-    // echo"</tr>";
-
-    // foreach($decoded as $deco =>$value){           
-    // var_dump($deco);
-    // echo"<tr>";
-    // echo"<td>$deco</td>";    
-    // echo"<td>$deco</td>";
-    // echo"<td>$deco</td>";
-    // echo"<td>$deco</td>";
-    // echo"<td>$deco</td>";
-    // }
-    // echo"</tr>";
+<?php    
 
 
+    $ch= curl_init();
+
+curl_setopt($ch, CURLOPT_URL,'https://reqres.in/api/users/');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,TRUE);
+curl_setopt($ch, CURLOPT_HEADER, false);
+
+$response=curl_exec($ch);
+
+if(curl_errno($ch)) echo curl_errno($ch);
+
+    else
+        $decoded = json_decode($response,true);        
+
+curl_close($ch);    
+
+foreach($decoded as $deco=>$value){  
+    echo"<table border=1>";
+    echo"<tr>";
+    echo"<td>id</td>";
+    echo"<td>email</td>";
+    echo"<td>first_name</td>";
+    echo"<td>last_name</td>";
+    echo"<td>avatar</td>";
+    echo"</tr>";
     
+    echo"<tr>";
+    echo "<td>";
+        echo $value[0]['id'];     
+        echo "</td>";
+        echo "<td>";
+        echo $value[0]['email'];
+        echo "</td>";
+        echo "<td>";
+        echo $value[0]['first_name'];
+        echo "</td>";
+        echo "<td>";
+        echo $value[0]['last_name'];
+        echo "</td>";
+        echo "<td>";
+        echo $value[0]['avatar'];        
+        echo "</td>"; 
+        echo "</tr>";  
+        echo "</table>";
+    } 
     ?>                     
 
     
