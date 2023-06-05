bcc358fa74115ee4fb8ef08c0b87f8d01c174544
unknown <coderschars289@gmail.com>
2023-06-05 00:59:14 -0600

    050623

--- consumir.php
+++ consumir.php
@@ -1,9 +1,9 @@
-<?php    
+<?php  
 
 
     $ch= curl_init();
 
-curl_setopt($ch, CURLOPT_URL,'https://reqres.in/api/users/');
+curl_setopt($ch, CURLOPT_URL,'https://reqres.in/api/users/2');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,TRUE);
 curl_setopt($ch, CURLOPT_HEADER, false);
 
@@ -14,37 +14,79 @@ if(curl_errno($ch)) echo curl_errno($ch);
     else
         $decoded = json_decode($response,true);        
 
-curl_close($ch);    
+curl_close($ch);         
+ ?>
+ <form action="principal.php" method="Get">
+ <table border=1>
+    <tr>
+        <td>Id</td>
+        <td>Email</td>
+        <td>First_name</td>
+        <td>Last_name</td>
+        <td>Avatar</td>
+</tr>
+<tr>
+            <td>
+                <input tipe="text" value="                
+                <?php 
+    foreach($decoded as $deco =>$value){        
+        if(isset(($value['id']))){
+            echo($value['id']);
+        }
+        
 
-foreach($decoded as $deco=>$value){  
-    echo"<table border=1>";
-    echo"<tr>";
-    echo"<td>id</td>";
-    echo"<td>email</td>";
-    echo"<td>first_name</td>";
-    echo"<td>last_name</td>";
-    echo"<td>avatar</td>";
-    echo"</tr>";
-    
-    echo"<tr>";
-    echo "<td>";
-        echo $value[0]['id'];     
-        echo "</td>";
-        echo "<td>";
-        echo $value[0]['email'];
-        echo "</td>";
-        echo "<td>";
-        echo $value[0]['first_name'];
-        echo "</td>";
-        echo "<td>";
-        echo $value[0]['last_name'];
-        echo "</td>";
-        echo "<td>";
-        echo $value[0]['avatar'];        
-        echo "</td>"; 
-        echo "</tr>";  
-        echo "</table>";
-    } 
-    ?>                     
+    }
+    ?>" name="id">
+            </input>
+            </td>
+            <td>
+            <input tipe="text" value="                
+                <?php 
+    foreach($decoded as $deco =>$value){    
+        if(isset($value['email'])){
+            echo($value['email']);
+        }    
+        
+    }
+    ?>" name="email">
+            </td>
+            <td>
+            <input tipe="text" value="                
+                <?php 
+    foreach($decoded as $deco =>$value){ 
+        if(isset($value['first_name'])){
+            echo $value['first_name'];
+        }       
+        
+    }
+    ?>" name="first_name">
+            </td>
+            <td>
+            <input tipe="text" value="                
+                <?php 
+    foreach($decoded as $deco =>$value){   
+        if(isset($value['last_name'])){
+            echo $value['last_name'];
+        }     
+        
+    }
+    ?>" name="last_name">
+            </td>
+            <td>
+            <input tipe="text" value="                
+                <?php 
+    foreach($decoded as $deco =>$value){ 
+        if(isset($value['avatar'])){
+            echo $value['avatar'];
+        }       
+        
+    }
+    ?>" name="avatar">
+    </td>        
+    </tr>
+</table>
+
+<input type="submit"></input>
+    </form>
 
     
--- principal.php
+++ principal.php
@@ -1,3 +1,41 @@
+
 <html>
-    <h1>Title</h1>
+    <h1>Datos Guardados</h1>
+<?php
+    $id=$_GET['id'];
+    $email=$_GET['email'];
+    $first_name=$_GET['first_name'];
+    $last_name=$_GET['last_name'];
+    $avatar=$_GET['avatar'];
+
+    $idc=trim($id);
+    $emailc=trim($email);
+    $first_namec=trim( $first_name);
+    $last_namec=trim($last_name);
+    $avatarc=trim($avatar);
+
+$servername = "localhost";
+$database = "consumirapi";
+$username = "root";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+ 
+$sql = "INSERT INTO users (id, email, first_name, last_name, avatar) 
+        VALUES ('$idc', '$emailc', '$first_namec', '$last_namec','$avatarc')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully".$emailc;
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+    ?>
+
 </html>
+    
